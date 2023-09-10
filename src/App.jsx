ac23e592277d12810bdd75d9703872b46906f650
albertarrebola08 <albertarrebola8@gmail.com>
2023-09-10 13:22:40 +0200

    componetizacion y separación de constantes del app.jsx y cambio de tipografia (importada sfprodisplay

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
 import { useState } from "react"
+import { Square } from "./components/Square"
+import confetti from "canvas-confetti"
+import { TURNS, WINNER_COMBOS } from './constants';
 
-const TURNS = {
-  X: 'x',
-  O: 'o',
-}
-
-const Square = ({children, isSelected, updateBoard, index}) =>{
-  const className = `square ${isSelected ? "is-selected" : ""}`
-  
-  const handleClick = () => {
-    updateBoard()
-  }
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
 
 function App() {
-  const [board,setBoard] = useState(Array(9).fill('*'))
+  const [board,setBoard] = useState(Array(9).fill(null))
   const [turn,setTurn] = useState(TURNS.X)
+  const [winner,setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    
+    for(const combo of WINNER_COMBOS){
+      const [a,b,c] = combo
+      if(
+        boardToCheck[a] && 
+        boardToCheck[a] === boardToCheck[b] && 
+        boardToCheck[a] === boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
+
+  const resetGame = () =>{
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  const checkEndGame = (newBoard) =>{
+    return newBoard.every((square) => square !== null)
+  }
+
+
   //funcion que cambia el turno
-  const updateBoard = () => {
+  const updateBoard = (index) => {
+    if(board[index] || winner) return
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    //miramos si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      confetti()
+      setWinner(newWinner)
+      
+    }//si hay un empate
+    else if (checkEndGame(newBoard)){
+      setWinner(false)
+    }
+    
+
+
   }
 
   return (
     <main className="board">
       <h1>Tic tac toe</h1>
+      <button onClick={resetGame}>Try again</button>
       <section className="game">
         {
           board.map((_, index) => {
@@ -46,14 +80,44 @@ function App() {
         }
 
       </section>
-      <section className="turn">
-        <Square isSelected={turn === TURNS.X}>
-          {TURNS.X}
-        </Square>
-        <Square isSelected={turn === TURNS.O}>
-          {TURNS.O}  
-        </Square>       
+      <section className="turn" style={{gap:'50px'}}>
+        <div>
+          <h2 style={{marginTop:'10px', marginBottom:'10px'}}>Player 1</h2>
+          <Square isSelected={turn === TURNS.X}>
+            {TURNS.X}
+          </Square>
+        </div>
+        
+        <div>
+          <h2 style={{marginTop:'10px', marginBottom:'10px'}}>Player 2</h2>
+          <Square isSelected={turn === TURNS.O}>
+            {TURNS.O}
+          </Square>
+        </div>      
       </section>
+
+      {
+        winner !== null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {
+                  winner === false 
+                  ? 'Empate'
+                  : 'Ha ganado: ' 
+                }
+              </h2>
+              <header className="win">
+                {winner && <Square>{winner}</Square>}
+              </header>
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
+      
     </main>
   )
     
